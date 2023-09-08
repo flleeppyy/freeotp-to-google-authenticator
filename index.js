00d096ff020acb6484ece9b0bf070e9c81f77286
flleeppyy <flleeppyy@gmail.com>
2023-09-08 03:27:02 -0700

    fix: wipe accounts table instead - remove whitespace and useless imports

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 // @ts-check
-const fs = require("node:fs");
-const child_process = require("node:child_process");
 const path = require("node:path");
 const base32 = require("hi-base32");
 const { AsyncDatabase } = require("promised-sqlite3");
 const inputJson = require("./freeotp.json");
 
-let updatedCount = 0;
-let newCount = 0;
+let tokensCount = 0;
+
 async function run() {
   const sql = await AsyncDatabase.open("./databases");
-
   for (const tokenJson of inputJson.tokens) {
     const token = {
       title: inputJson.tokenOrder[inputJson.tokens.indexOf(tokenJson)],
@@ -18,27 +15,17 @@ async function run() {
     };
 
     try {
-      const result = await sql.get(`SELECT secret FROM "accounts" WHERE secret = ('${token.secret}')`);
-      if (result?.secret) {
-        await sql.run(`UPDATE "main"."accounts" SET (email, original_name) = ('${token.title}','${token.title}') WHERE "secret"='${token.secret}'`);
-        updatedCount++
-        continue;
-      }
-      newCount++
+      await sql.run(`DELETE FROM "main"."accounts"`);
+      tokensCount++
       await sql.run(`INSERT INTO "main"."accounts"("_id","email","secret","type","original_name") VALUES (NULL,'${token.title}','${token.secret}',0,"${token.title}");`)
-      // await sqlRunPromise();
     } catch (e) {
       console.error("Error running SQL");
       console.error(e);
     }
   }
 
-  console.log(`Updated ${updatedCount} tokens and added ${newCount} new ones`)
-
+  console.log(`Cleared tokens in database and added ${tokensCount} new ones.`)
   sql.close();
 }
 
-
-
 run();
-
